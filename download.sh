a72fc956bc1f3febc032898b7c487125fc6503a0
仲夏叶 <stornado@qq.com>
2020-06-11 23:09:54 +0800

    feat: add download script

---
+++ download.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7015android1680.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7015android1680_arm64.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7014android1660.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7014android1660_arm64.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7013android1640.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7013android1640_arm64.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7012android1620.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7012android1620_arm64.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7011android1600.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7011android1600_arm64.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7010android1580.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin7010android1580_arm64.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin709android1560.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin709android1560_arm64.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin708android1540.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin707android1520.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin706android1460.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin705android1440.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin704android1420.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin703android1400.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin700android1380.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin673android1360.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin672android1340.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin667android1320.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin666android1300.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin665android1280.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin663android1260.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin662android1240.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin661android1220.apk
+curl -O -L -C - http://dldir1.qq.com/weixin/android/weixin661android1220_1.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin660android1200.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin6523android1180.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin6522android1160.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin6519android1140.apk
+curl -O -L -C - https://dldir1.qq.com/weixin/android/weixin6516android1120.apk
+
+
